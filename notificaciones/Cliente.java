a0780349c5e9e867f903fbc0c0a90962415e4a21
Silvina Laurent <silvinalaurent@gmail.com>
2026-03-12 07:15:48 -0300

    eliminedni

--- notificaciones/Cliente.java
+++ notificaciones/Cliente.java
@@ -1,23 +1,22 @@
 public class Cliente { 
-    private String nombre; 
-    private String email; 
-    private String telefono; 
-    private String dni;
+    String nombre; 
+    String email; 
+    String telefono; 
     private boolean prefiereEmail; 
     private boolean prefiereSMS; 
      
-    public Cliente(String nombre, String email, String telefono, String dni) { 
+    public Cliente(String nombre, String email, String telefono) { 
         this.nombre = nombre; 
         this.email = email; 
         this.telefono = telefono; 
-        this.dni = dni;
+     
     } 
      
     // Getters y setters 
     public String getNombre() { return nombre; } 
     public String getEmail() { return email; } 
     public String getTelefono() { return telefono; } 
-    public String getDni() { return dni; } 
+    
     
     public void setPreferencias(boolean email, boolean sms) { 
         this.prefiereEmail = email; 

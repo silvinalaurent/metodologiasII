723181cc8b409a16df2b1f231567ceb634512165
Silvina Laurent <silvinalaurent@gmail.com>
2026-03-12 07:11:32 -0300

    agregodni

--- notificaciones/Cliente.java
+++ notificaciones/Cliente.java
@@ -2,20 +2,23 @@ public class Cliente {
     private String nombre; 
     private String email; 
     private String telefono; 
+    private String dni;
     private boolean prefiereEmail; 
     private boolean prefiereSMS; 
      
-    public Cliente(String nombre, String email, String telefono) { 
+    public Cliente(String nombre, String email, String telefono, String dni) { 
         this.nombre = nombre; 
         this.email = email; 
         this.telefono = telefono; 
+        this.dni = dni;
     } 
      
     // Getters y setters 
     public String getNombre() { return nombre; } 
     public String getEmail() { return email; } 
     public String getTelefono() { return telefono; } 
-     
+    public String getDni() { return dni; } 
+    
     public void setPreferencias(boolean email, boolean sms) { 
         this.prefiereEmail = email; 
         this.prefiereSMS = sms; 

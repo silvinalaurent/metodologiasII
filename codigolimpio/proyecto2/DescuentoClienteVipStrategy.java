67015ec825a232f2d231cba6afce76cf97469e68
Silvina Laurent <66426031+silvinalaurent@users.noreply.github.com>
2026-03-30 11:04:13 -0300

    Create DescuentoClienteVipStrategy.java

---
+++ codigolimpio/proyecto2/DescuentoClienteVipStrategy.java
@@ -0,0 +1,25 @@
+public class DescuentoClienteVipStrategy implements EstrategiaDescuento { 
+    private static final double TASA_DESCUENTO_VIP = 0.10; 
+    private static final int CANTIDAD_MINIMA_COMPRAS_DESCUENTO_EXTRA = 5; 
+    private final HistorialComprasCliente historialCompras; 
+     
+    public DescuentoClienteVipStrategy(HistorialComprasCliente historialCompras) { 
+        this.historialCompras = historialCompras; 
+    } 
+     
+    @Override 
+    public double calcular(double subtotal) { 
+        double descuento = subtotal * TASA_DESCUENTO_VIP; 
+         
+        if (tieneAltaFrecuenciaDeCompras()) { 
+            descuento += subtotal * 0.05; // 5% extra por frecuencia 
+        } 
+         
+        return descuento; 
+    } 
+     
+    private boolean tieneAltaFrecuenciaDeCompras() { 
+        return historialCompras.obtenerCantidadComprasUltimoMes() >= CANTIDAD_MINIMA_COMPRAS_DESCUENTO_EXTRA; 
+    } 
+} 
+ 

6398410752043206a0e9b49f60d553aa015bc02c
Silvina Laurent <66426031+silvinalaurent@users.noreply.github.com>
2026-03-30 16:35:04 -0300

    Update DescuentoClienteVipStrategy.java

--- codigolimpio/proyecto2/DescuentoClienteVipStrategy.java
+++ codigolimpio/proyecto2/DescuentoClienteVipStrategy.java
@@ -1,6 +1,6 @@
 public class DescuentoClienteVipStrategy implements EstrategiaDescuento { 
     private static final double TASA_DESCUENTO_VIP = 0.10; 
-    private static final int CANTIDAD_MINIMA_COMPRAS_DESCUENTO_EXTRA = 5; 
+    private static final int CANTIDAD_MINIMA_COMPRAS_DESCUENTO_EXTRA = 3; 
     private final HistorialComprasCliente historialCompras; 
      
     public DescuentoClienteVipStrategy(HistorialComprasCliente historialCompras) { 
